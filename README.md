9995ced2c796c9cc3e438aed9dd2565b08758ef7
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-07-09 11:30:15 +0200

    convert to python package

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
-# Airflow and TM1 
+# airflow-tm1
 
-Proof of concept that demonstrates using a basic custom hook to create a connection to a TM1 database and uses this in an example DAG to extract data from a view and write it as a csv to an S3 bucket
-
-## Requirements
-
-* An Airflow environment with support for S3 and TM1py
-* Example DAG depends on connections "s3_default" and "tm1_default"
+A package that provides a hook to simplify connecting to the IBM Cognos TM1 / Planning Analytics REST API.
 
 
 
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="airflow-tm1",
+    version="0.0.1",
+    author="Alexander Sutcliffe",
+    author_email="sutcliffe.alex@gmail.com",
+    description="A package to simplify connecting to the TM1 REST API from Apache Airflow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scrambldchannel/airflow-tm1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+                    "apache-airflow",
+                     "TM1py",
+                     ],
+)
