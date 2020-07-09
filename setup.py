0a6fcbf686d383edae6c7e3df19bbd25b31fc615
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-07-09 15:45:24 +0200

    Set dev status to beta

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     url="https://github.com/scrambldchannel/airflow-tm1",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
