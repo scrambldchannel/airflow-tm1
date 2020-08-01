43ba4396e44f7b7121acd54bbab04375ad01f4c1
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-08-01 10:17:52 +0200

    cleanup

--- example_dags/tm1_to_s3.py
+++ example_dags/tm1_to_s3.py
@@ -15,32 +15,28 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id="tm1_to_s3_example",
+    dag_id="view_to_s3_example",
     default_args=default_args,
 )
-
-#dag_config = Variable.get("tm1_to_s3_example_config", deserialize_json=True)
 
 
 def cube_view_to_s3(cube, view, **kwargs):
 
-    # create the hook and initialise the instance of the TM1Service object
     tm1_hook = TM1Hook(tm1_conn_id="tm1_default")
-
     tm1 = tm1_hook.get_conn()
 
-    # pull data from a view
     view_data = tm1.cubes.cells.execute_view_csv(
         cube_name=cube, view_name=view, private=False)
-    #s3_hook = S3Hook(aws_conn_id="s3_default")
+    s3_hook = S3Hook(aws_conn_id="s3_default")
 
-    # write file to s3
- #   s3_hook.load_string(string_data=view_data, key=key, bucket_name=bucket, replace=True)
+    s3_hook.load_string(string_data=view_data, key=key,
+                        bucket_name=bucket, replace=True)
 
 
 t1 = PythonOperator(
-    task_id="read_from_TM1",
+    task_id="cube_from_TM1",
     python_callable=cube_view_to_s3,
-    op_kwargs={"cube": "Revenue", "view": "zExport Data"},
+    op_kwargs={"cube": "Revenue", "view": "zExport Data",
+               "key": "Revenue", "bucket": "airflow-test"},
     dag=dag,
 )
