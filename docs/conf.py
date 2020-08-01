dde7cb0c155e9742f39f7c65d1e97468fa1544a9
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-08-01 21:35:00 +0200

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Alexander Sutcliffe"
 author = "Alexander Sutcliffe"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.7"
+release = "0.0.8"
 
 
 # -- General configuration ---------------------------------------------------
