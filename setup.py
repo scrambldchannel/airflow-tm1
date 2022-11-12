93a22a035abadc241967ebcd7716917fe00ec971
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-11-12 11:03:25 +0100

    chore: revert

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="airflow-tm1",
-    version="0.0.8",
+    version="0.0.11",
     author="Alexander Sutcliffe",
     author_email="sutcliffe.alex@gmail.com",
     description="A package to simplify connecting to the TM1 REST API from Apache Airflow",
